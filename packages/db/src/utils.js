84ceb8beabd003f1680e9353bbd3ce09352fc815
Lodewijk Wensveen <lodewijkwensveen@protonmail.com>
2025-08-06 18:09:36 +0700

    chore(db): clean up compiled outputs and keep only source files

--- packages/db/src/utils.js
+++
@@ -1,9 +0,0 @@
-import { timestamp } from 'drizzle-orm/pg-core';
-export const defaultTimestampOptions = {
-    withTimezone: true,
-    mode: 'date',
-};
-export const createTimestampColumn = (columnName, isUpdatedColumn = false) => {
-    const column = timestamp(columnName, defaultTimestampOptions).notNull().defaultNow();
-    return isUpdatedColumn ? column.$onUpdateFn(() => new Date()) : column;
-};
