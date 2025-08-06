aba20fddcc3c27445217ac7fe598424658449da1
Lodewijk Wensveen <lodewijkwensveen@protonmail.com>
2025-08-06 18:09:30 +0700

    chore(types): export seller-batches and warehouse-pickup schemas in index.ts

--- packages/types/src/index.ts
+++ packages/types/src/index.ts
@@ -1,4 +1,6 @@
 export * from './events/schemas.js';
 export * from './pools/schemas.js';
 export * from './schemas.js';
+export * from './seller-batches/schemas.js';
+export * from './warehouse-pickup/schemas.js';
 export * from './webhooks/schemas.js';
