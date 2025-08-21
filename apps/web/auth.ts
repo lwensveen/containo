66e59881ff2d829ecedc4fa1736d971e2f385952
Lodewijk Wensveen <lodewijkwensveen@protonmail.com>
2025-08-21 21:51:48 +0700

    chore(auth): remove unconfigured social providers (facebook/google)

--- apps/web/auth.ts
+++ apps/web/auth.ts
@@ -50,16 +50,6 @@ export const auth: any = betterAuth({
       // additionalCookies: ['custom_cookie'],
     },
   },
-  socialProviders: {
-    facebook: {
-      clientId: process.env.FACEBOOK_CLIENT_ID as string,
-      clientSecret: process.env.FACEBOOK_CLIENT_SECRET as string,
-    },
-    google: {
-      clientId: process.env.GOOGLE_CLIENT_ID as string,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-    },
-  },
   account: {
     accountLinking: {
       enabled: true,
